2bbaf9292ddc34fd07e10abad1b33ba938c7ae07
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-12-04 22:11:13 +0700

    Fixing Prop Drilling With Composition (And Building a Layout)

--- src/App.js
+++ src/App.js
@@ -45,12 +45,11 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-function Navbar({ movies }) {
+function Navbar({ children }) {
     return (
         <nav className="nav-bar">
             <Logo />
-            <Search />
-            <NumResults movies={movies} />
+            {children}
         </nav>
     );
 }
@@ -85,13 +84,8 @@ function NumResults({ movies }) {
     );
 }
 
-function Main({ movies }) {
-    return (
-        <main className="main">
-            <ListBox movies={movies} />
-            <WatchedBox />
-        </main>
-    );
+function Main({ children }) {
+    return <main className="main">{children}</main>;
 }
 
 export default function App() {
@@ -99,13 +93,21 @@ export default function App() {
 
     return (
         <>
-            <Navbar movies={movies} />
-            <Main movies={movies} />
+            <Navbar>
+                <Search />
+                <NumResults movies={movies} />
+            </Navbar>
+            <Main>
+                <ListBox>
+                    <MovieList movies={movies} />
+                </ListBox>
+                <WatchedBox />
+            </Main>
         </>
     );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
     const [isOpen1, setIsOpen1] = useState(true);
 
     return (
@@ -116,7 +118,7 @@ function ListBox({ movies }) {
             >
                 {isOpen1 ? "–" : "+"}
             </button>
-            {isOpen1 && <MovieList movies={movies} />}
+            {isOpen1 && children}
         </div>
     );
 }
