5a57a5b9611b9016b7fc3a4fa87f2673388e2927
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-12-23 19:50:03 +0700

    Initializing State With a Callback (Lazy Initial State)

--- src/App.js
+++ src/App.js
@@ -51,11 +51,16 @@ function Main({ children }) {
 export default function App() {
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
     const [selectedId, setSelectedId] = useState(null);
     // const tempQuery = 'interstellar';
+
+    // const [watched, setWatched] = useState([])
+    const [watched, setWatched] = useState(function () {
+        const storedValue = localStorage.getItem('watched');
+        return JSON.parse(storedValue);
+    });
 
     function handleSelectedMovie(id) {
         setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -67,11 +72,20 @@ export default function App() {
 
     function handleAddWatched(movie) {
         setWatched((watched) => [...watched, movie]);
+
+        // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
     }
 
     function handleDeleteWatched(id) {
         setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
     }
+
+    useEffect(
+        function () {
+            localStorage.setItem('watched', JSON.stringify(watched));
+        },
+        [watched]
+    );
 
     useEffect(
         function () {
