5cc1f0e5948d6a4543c0ea58edba68a66d8a1d5d
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-12-25 19:43:10 +0700

    Refs to Select DOM elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import StarRating from './StarRating';
 
 const average = (arr) =>
@@ -23,11 +23,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-    useEffect(function () {
-        const el = document.querySelector('.search');
-        console.log(el);
-        el.focus();
-    }, []);
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        function callback(e) {
+            if (document.activeElement === inputEl.current) return;
+
+            if (e.code === 'Enter') {
+                inputEl.current.focus();
+                setQuery('');
+            }
+        }
+        document.addEventListener('keydown', callback);
+        return () => document.addEventListener('keydown', callback);
+    }, [setQuery]);
+
+    // useEffect(function () {
+    //     const el = document.querySelector('.search');
+    //     console.log(el);
+    //     el.focus();
+    // }, []);
 
     return (
         <input
@@ -36,6 +51,7 @@ function Search({ query, setQuery }) {
             placeholder='Search movies...'
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
         />
     );
 }
