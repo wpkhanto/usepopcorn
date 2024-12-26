ad3900c6777e00f4b99030ccb83b61a070329128
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-12-26 23:19:18 +0700

    Refs to Persist Data Between Renders

--- src/App.js
+++ src/App.js
@@ -286,6 +286,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     const [isLoading, setIsLoading] = useState(false);
     const [userRating, setUserRating] = useState('');
 
+    const countRef = useRef(0);
+    let count = 0;
+
+    useEffect(
+        function () {
+            if (userRating) countRef.current++;
+            if (userRating) count++;
+        },
+        [userRating, count]
+    );
+
     const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
     const watchedUserRating = watched.find(
         (movie) => movie.imdbID === selectedId
@@ -316,6 +327,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
             imdbRating: Number(imdbRating),
             runtime: Number(runtime.split(' ').at(0)),
             userRating,
+            countRatingCecisions: countRef.current,
+            count: count,
         };
         onAddWatched(newWatchedMovie);
         onCloseMovie();
